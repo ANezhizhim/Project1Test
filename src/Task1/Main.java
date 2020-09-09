c47254c187253f7e202e441d67b3092437051d2f
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-09 13:24:17 +0300

    Коммит11

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -7,7 +7,7 @@ public class Main {
     Phone Phone1 = new Phone("9155476161","Honor", 120.1f);
     Phone Phone2 = new Phone("9155476162","Samsung", 125.2f);
     Phone Phone3 = new Phone("9155476163","HTC", 127.3f);
-    Phone Phone4 = new Phone("9155476164","Nokia");
+    Phone Phone4 = new Phone("9155476165","Nokia");
 
     Phone1.receiveCall("Вася");
     System.out.println("Номер телефона: " + Phone1.number + " модель: "+Phone1.model + " Вес: "+Phone1.weight);
@@ -18,6 +18,8 @@ public class Main {
     System.out.println("Номер телефона:" + Phone2.getNumber());
     System.out.println("Номер телефона:" + Phone3.getNumber());
     System.out.println("Номер телефона:" + Phone4.getNumber());
+
+
 
         Phone1.receiveCall("Давид Маркович","Городской");
         Phone1.sendMessage1("9155476161","9155476162","9155476163");
