f2338780ae2e20b3fa9a279c935c051ee78028b5
A.Nezhizhim <A.Nezhizhim@x5.ru>
2020-09-22 15:07:33 +0300

    Исправлена 1-я задача

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -7,10 +7,10 @@ public class Main {
         Phone arrPhone[] = new Phone[4];
 
 
-        arrPhone[ 0] =new Phone("9155476161", "Honor", 120.1f);
-        arrPhone[ 1] =new Phone("9155476162", "Samsung", 125.2f);
-         arrPhone[ 2]=new Phone("9155476163", "HTC", 127.3f);
-        arrPhone[ 3] =new Phone("9155476165", "Nokia");
+        arrPhone[0] = new Phone("9155476161", "Honor", 120.1f);
+        arrPhone[1] = new Phone("9155476162", "Samsung", 125.2f);
+        arrPhone[2] = new Phone("9155476163", "HTC", 127.3f);
+        arrPhone[3] = new Phone("9155476165", "Nokia");
 
         arrPhone[0].receiveCall("Вася");
         for (Phone phon :
@@ -19,22 +19,11 @@ public class Main {
 
         }
 
-        // System.out.println("Номер телефона: " + Phone1.number + " модель: "+Phone1.model + " Вес: "+Phone1.weight);
-        // System.out.println("Номер телефона: " + Phone2.number + " модель: "+Phone2.model + " Вес: "+Phone2.weight);
-        // System.out.println("Номер телефона: " + Phone3.number + " модель: "+Phone3.model + " Вес: "+Phone3.weight);
         for (Phone phon :
                 arrPhone) {
             System.out.println("Номер телефона:" + phon.getNumber());
         }
-  /*  System.out.println("Номер телефона:" + Phone1.getNumber());
-    System.out.println("Номер телефона:" + Phone2.getNumber());
-    System.out.println("Номер телефона:" + Phone3.getNumber());
-    System.out.println("Номер телефона:" + Phone4.getNumber());
-*/
 
-
-        //    Phone1.receiveCall("Давид Маркович","Городской");
-        // Phone1.sendMessage1("9155476161","9155476162","9155476163");
         arrPhone[0].receiveCall("Давид Маркович", "Городской");
         arrPhone[0].sendMessage1("9155476161", "9155476162", "9155476163");
 
